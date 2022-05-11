80d0cc98bf61a268ff76ab8bbc49296d54dc5708
Alabs02 <alabson.inc@gmail.com>
2022-05-11 12:40:55 +0100

    added helper classes

---
+++ src/layouts/auth-layout/AuthLayout.tsx
@@ -0,0 +1,12 @@
+import React, {  Fragment } from 'react';
+import { Outlet } from 'react-router-dom';
+
+const AuthLayout = () => {
+  return (
+    <Fragment>
+        {/* <Outlet /> */}
+    </Fragment>
+  );
+}
+
+export { AuthLayout as default };
--- src/router/authRoutes.tsx
+++ src/router/authRoutes.tsx
@@ -1,14 +1,14 @@
 import { Route } from 'react-router-dom';
 
 // LAYOUT
-// import { AuthLayout } from 'src/layouts';
+import { AuthLayout } from 'src/layouts';
 
 // PAGES
 import { Signin, Signup } from 'src/pages/auth';
 
 const AuthRoutes = () => {
   return (
-    <Route>
+    <Route element={<AuthLayout />}>
       <Route path="signin" element={<Signin />}></Route>
       <Route path="signup" element={<Signup />}></Route>
     </Route>
